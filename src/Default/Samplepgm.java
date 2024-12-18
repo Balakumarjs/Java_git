a81eccb8ad508e98addced8b372f069389f97661
Balakumarjs <balakumarvcet@gmail.com>
2024-12-18 10:37:14 +0530

    First commit

---
+++ src/Default/Samplepgm.java
@@ -0,0 +1,17 @@
+package Default;
+
+import Default.Samplepgm;
+
+public class Samplepgm {
+	  public void m1()
+	    {
+	    	System.out.println("java");
+	    }
+		
+	public static void main(String[] args) 
+	{
+		Samplepgm a = new Samplepgm();
+		a.m1();
+	}
+
+}
